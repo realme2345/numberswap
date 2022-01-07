48d39097922c18007b4efdbc2e7748b2e7b2dc50
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-07 13:14:21 +0530

    addiding program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raja_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome");
+           // ReplaceString.ReadingInputFromUser();
+            NumberSwap.ReadingInputFromUser();
+        }
+    }
+}
